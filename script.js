71e4807dd793dea33bd4a0b58fb186f13ab4bde3
Beksultan <beka47674@gmail.com>
2024-11-11 10:48:15 +0500

    examps

---
+++ script.js
@@ -0,0 +1,55 @@
+// let course1 = +prompt("1-ден 12-ге дейынгы сан енгизиниз")
+// let course2 = +prompt("1-ден 12-ге дейынгы сан енгизиниз")
+// let course3 = +prompt("1-ден 12-ге дейынгы сан енгизиниз")
+// let num = 0
+
+// switch (true) {
+//     case course1 && course2 > course3 && course3:
+//         alert("Ай 3,Маусым:Spring:" + course1) 
+//         break;
+//      case course2 > course1 && course2 > course3:
+//         alert("Ай 5,Маусым:Spring:" + course2)
+//         break;
+//     case course3 > course2 && course3 > course1:
+//         alert("Ай 12,Маусым:Winter" + course3)
+//         break;
+// }
+
+
+
+
+// let a = +prompt("а санын енгизиниз")
+// let b = +prompt("b санын енгизиниз")
+
+// if (true) {
+//     alert("a саны b санынан киши")
+//     alert("a саны b сандарынын орташасы :32.5")
+// } else {
+    
+// }
+
+
+
+// let san1 = +prompt("Биринши санды енгизиниз:")
+
+// switch (true) {
+//     case san1:
+//         alert("Ен улкен жане кишкене саннын айырмасы: 8 ")
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+// let a =+prompt("1-san")
+// let b =+prompt("2-san")
+// let c =+prompt("3-san")
+
+// if (a+b && a+c) {
+//     alert("Ен улкен жане кишкене саннын айырмасы: 8 ") + (a+b) || (a+c)
+// } else {
+//     alert("Ен улкен жане кишкене саннын айырмасы: 12 ") + (b+a) || (b+c)
+// }
